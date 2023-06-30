faf5474b3bbceeab88ef51b115a44376194d4efc
rakshitmehra <rakshitmehra88@gmail.com>
2023-07-01 01:49:34 +0530

    Certificate-Downloader

---
+++ main.js
@@ -0,0 +1,11 @@
+function func(){
+    let url = document.getElementById("name").value;
+    if(!url) {
+        alert("Please enter your name");
+        return;
+    } 
+
+    document.getElementById("btn").href = "./certificates/" + url + ".pdf";
+    document.getElementById("btn").download =  url + "-certificate.pdf";
+    document.getElementById("btn").click();
+}
